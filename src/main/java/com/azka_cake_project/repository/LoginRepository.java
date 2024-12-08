e839e796e45ada6f1a153d9e9034a3edf1c484df
unknown <fajardillah25@gmail.com>
2024-12-08 20:00:32 +0700

    Upload

---
+++ src/main/java/com/azka_cake_project/repository/LoginRepository.java
@@ -0,0 +1,10 @@
+package com.azka_cake_project.repository;
+
+import com.azka_cake_project.entity.Login;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LoginRepository extends JpaRepository<Login, String> {
+
+
+
+}
